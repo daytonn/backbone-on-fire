e24e1c6c226621fb6b81adecc84e827249de377e
Dayton Nolan <daytonn@gmail.com>
2014-06-29 14:09:48 -0500

    Fix nested model serializer to add to existing collections

--- spec/nested_model_serializer_spec.js
+++ spec/nested_model_serializer_spec.js
@@ -11,7 +11,9 @@ describe("NestedModelSerializer", function() {
 
   beforeEach(function() {
     ChildModel = Backbone.OnFire.Model.extend();
-    ChildCollection = Backbone.Collection.extend();
+    ChildCollection = Backbone.Collection.extend({
+      model: ChildModel
+    });
     TestModel = Backbone.OnFire.Model.extend({
       urlRoot: "tests",
       serializers: [new Backbone.OnFire.NestedModelSerializer],
@@ -21,23 +23,30 @@ describe("NestedModelSerializer", function() {
         child_collection: ChildCollection
       }
     });
-    firstChildAttributes = { id: 2, name: "my first child" };
-    secondChildAttributes = { id: 5, name: "my second child" };
-    childCollectionAttributes = [
-      { id: 6, name: "child collection 6" },
-      { id: 7, name: "child collection 7" }
-    ];
-    attributes = {
-      first_child: firstChildAttributes,
-      second_child: secondChildAttributes,
-      child_collection: childCollectionAttributes
+    firstChildAttributes = function() {
+      return { id: 2, name: "my first child" };
     };
-    subject = new TestModel(_.clone(attributes));
+    secondChildAttributes = function() {
+      return { id: 5, name: "my second child" };
+    };
+    childCollectionAttributes = function() {
+      return [
+        { id: 6, name: "child collection 6" },
+        { id: 7, name: "child collection 7" }
+      ];
+    };
+    attributes = function() {
+      return {
+        first_child: firstChildAttributes(),
+        second_child: secondChildAttributes(),
+        child_collection: childCollectionAttributes()
+      };
+    };
+    subject = new TestModel(attributes());
   });
 
   afterEach(function() {
     data = undefined;
-    attributes = undefined;
   });
 
   describe("deserialize", function() {
@@ -55,6 +64,46 @@ describe("NestedModelSerializer", function() {
       expect(_(subject.attributes.first_child.attributes).size()).to.equal(2);
       expect(_(subject.attributes.second_child.attributes).size()).to.equal(2);
     });
+
+    describe("with existing relationships", function() {
+      describe("model", function() {
+        it("replaces the relationship", function() {
+          subject = new TestModel({ first_child: firstChildAttributes() });
+          var originalCID = subject.get("first_child").cid;
+          subject.set({ first_child: { id: 3, name: "my first child updated" }});
+          expect(subject.get("first_child").cid).not.to.equal(originalCID);
+          expect(subject.get("first_child").id).to.equal(3);
+          expect(subject.get("first_child").get("name")).to.equal("my first child updated");
+        });
+
+        it("replaces instances", function() {
+          subject = new TestModel({ first_child: firstChildAttributes() });
+          var originalCID = subject.get("first_child").cid;
+          subject.set({ first_child: new ChildModel({ id: 3, name: "my first child updated" }) });
+          expect(subject.get("first_child").cid).not.to.equal(originalCID);
+          expect(subject.get("first_child").id).to.equal(3);
+          expect(subject.get("first_child").get("name")).to.equal("my first child updated");
+        });
+      });
+
+      describe("collection", function() {
+        it("adds to the existing colleciton", function() {
+          subject = new TestModel({ child_collection: childCollectionAttributes() });
+          var originalCID = subject.get("child_collection").cid;
+          subject.set({ child_collection: { id: 8, name: "child collection 8" }});
+          expect(subject.get("child_collection").cid).to.equal(originalCID);
+          expect(subject.get("child_collection").size()).to.equal(3);
+          expect(subject.get("child_collection").last().id).to.equal(8);
+        });
+
+        it("replaces instances", function() {
+          subject = new TestModel({ child_collection: childCollectionAttributes() });
+          subject.set({ child_collection: new ChildCollection([{ id: 8, name: "child collection 8" }]) });
+          expect(subject.get("child_collection").size()).to.equal(1);
+          expect(subject.get("child_collection").first().id).to.equal(8);
+        });
+      });
+    });
   });
 
   describe("toJSON", function() {
@@ -64,7 +113,6 @@ describe("NestedModelSerializer", function() {
       expect(json.second_child).to.eql(subject.get("second_child").toJSON());
       expect(json.child_collection).to.eql(subject.get("child_collection").models.map(function(model) { return model.toJSON(); }));
     });
-
   });
 
   describe("serialize", function() {
@@ -74,9 +122,9 @@ describe("NestedModelSerializer", function() {
     });
 
     it("creates nested attributes", function() {
-      expect(data.first_child_attributes).to.eql(firstChildAttributes);
-      expect(data.second_child_attributes).to.eql(secondChildAttributes);
-      expect(data.child_collection_attributes).to.eql(childCollectionAttributes);
+      expect(data.first_child_attributes).to.eql(firstChildAttributes());
+      expect(data.second_child_attributes).to.eql(secondChildAttributes());
+      expect(data.child_collection_attributes).to.eql(childCollectionAttributes());
 
       expect(_.isUndefined(data.first_child)).to.equal(true);
       expect(_.isUndefined(data.second_child)).to.equal(true);
